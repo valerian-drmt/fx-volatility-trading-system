cdc08c881c1b2d0f634911da18f10ab013fec5cb
valerian-drmt <119040048+valerian-drmt@users.noreply.github.com>
2025-05-25 15:12:52 +0200

    Update

--- Project/Config/LoggerConfig.py
+++ Project/Config/LoggerConfig.py
@@ -1,26 +1,25 @@
 import logging
 import colorlog
 
+def colored_logger(level=logging.INFO):
+    handler = colorlog.StreamHandler()
+    handler.setFormatter(colorlog.ColoredFormatter(
+        fmt="%(log_color)s[%(asctime)s] %(levelname)s - %(filename)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        log_colors={
+            'DEBUG':    'white',
+            'INFO':     'light_green',
+            'WARNING':  'yellow',
+            'ERROR':    'light_red',
+            'CRITICAL': 'bold_red',
+        },
+        reset=True,
+        secondary_log_colors={},
+        style='%'
+    ))
 
-def colored_logger(name: str = None, level=logging.INFO):
-    logger = colorlog.getLogger(name)
+    logger = colorlog.getLogger()
     logger.setLevel(level)
-
-    # Avoid duplicate handlers if already configured
-    if not logger.hasHandlers():
-        handler = colorlog.StreamHandler()
-        handler.setFormatter(colorlog.ColoredFormatter(
-            fmt="%(log_color)s[%(asctime)s] %(levelname)s - %(filename)s - %(message)s",
-            datefmt="%Y-%m-%d %H:%M:%S",
-            log_colors={
-                'DEBUG': 'white',
-                'INFO': 'light_green',
-                'WARNING': 'yellow',
-                'ERROR': 'light_red',
-                'CRITICAL': 'bold_red',
-            },
-            style='%'
-        ))
-        logger.addHandler(handler)
-
+    logger.handlers.clear()
+    logger.addHandler(handler)
     return logger
