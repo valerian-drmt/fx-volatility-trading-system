b2d1db8dfaceb3e4debb199301e9b882b5a3efc7
valerian-drmt <119040048+valerian-drmt@users.noreply.github.com>
2025-06-07 16:15:55 +0200

    Update

--- Project/Data/Labels.py
+++ Project/Data/Labels.py
@@ -1,6 +1,7 @@
 # 🔧 Config import
 import sys
 import os
+
 current_dir = os.path.dirname(os.path.abspath(__file__))
 project_root = os.path.abspath(os.path.join(current_dir, '..'))
 sys.path.append(project_root)
