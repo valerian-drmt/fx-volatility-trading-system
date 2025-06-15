c0a6a9f75d94290912a5cd29401f01d69aeaabd2
valerian-drmt <119040048+valerian-drmt@users.noreply.github.com>
2025-06-15 15:02:25 +0200

    Update

--- Project/Data/DataFetcher.py
+++ Project/Data/DataFetcher.py
@@ -3,12 +3,9 @@ import ccxt
 import pandas as pd
 
 # 🔧 Config import
-import sys
 import os
-current_dir = os.path.dirname(os.path.abspath(__file__))
-project_root = os.path.abspath(os.path.join(current_dir, '..'))
-sys.path.append(project_root)
 from Config.LoggerConfig import colored_logger
+
 logger = colored_logger()
 current_file = os.path.basename(__file__)
 logger.info(f"Logger initialized ({current_file})")
