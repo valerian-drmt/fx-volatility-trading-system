b00387b684e998eee19ef921b0dd2ac40cb8cc00
valerian-drmt <119040048+valerian-drmt@users.noreply.github.com>
2025-06-29 17:14:01 +0200

    Update

--- Project/core/data/fetcher.py
+++ Project/core/data/fetcher.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 import ccxt
 import pandas as pd
+from io import BytesIO
+import requests
+import gzip
 
 # 🔧 config import
 import os
@@ -17,7 +20,7 @@ class DataFetcher:
         self.interval = interval
         self.raw_data = None
 
-    def get_binance_data(self):
+    def get_binance_data_ohlcv(self):
         binance = ccxt.binance({
             'options': {'defaultType': 'future'},
             'enableRateLimit': True
@@ -72,33 +75,70 @@ class DataFetcher:
                 break
 
         if all_data:
-            df = pd.DataFrame(all_data, columns=['Timestamp', 'Open', 'High', 'Low', 'Close', 'Volume'])
-            df['Timestamp'] = pd.to_datetime(df['Timestamp'], unit='ms')
-            df = df.drop_duplicates(subset='Timestamp').reset_index(drop=True)
+            df = pd.DataFrame(all_data, columns=['time', 'open', 'high', 'low', 'close', 'volume'])
+            df['time'] = pd.to_datetime(df['time'], unit='ms')
+            df = df.drop_duplicates(subset='time').reset_index(drop=True)
             self.raw_data = df
             logger.info(f"Successfully fetched {len(df)} candles in total.")
         else:
-            self.raw_data = pd.DataFrame(columns=['Timestamp', 'Open', 'High', 'Low', 'Close', 'Volume'])
+            self.raw_data = pd.DataFrame(columns=['time', 'open', 'high', 'low', 'close', 'volume'])
             logger.warning("No data was fetched — the resulting DataFrame is empty.")
 
         return self
 
-    def save_to_csv(self, directory="./"):
+    def get_bybit_data_dom(self):
+
+        base_url = f"https://public.bybit.com/spot/{self.symbol}/"
+        dates = pd.date_range(start=self.start_date, end=self.end_date, freq='D').strftime('%Y-%m-%d')
+        dfs = []
+
+        for date in dates:
+            fname = f"{self.symbol}_{date}.csv.gz"
+            url = base_url + fname
+            logger.info(f"Trying: {url}")
+            response = requests.get(url)
+            if response.status_code == 200:
+                logger.info(f"Downloaded: {fname}")
+                with gzip.GzipFile(fileobj=BytesIO(response.content)) as gz:
+                    temp_df = pd.read_csv(gz)
+                    temp_df['time'] = pd.to_datetime(temp_df['timestamp'], unit='ms')
+                    dfs.append(temp_df)
+            else:
+                logger.warning(f"File not found or not available for: {date}")
+
+        if dfs:
+            full_df = pd.concat(dfs, ignore_index=True)
+            drop_cols = [col for col in ['timestamp', 'rpi', 'id'] if col in full_df.columns]
+            full_df = full_df.drop(columns=drop_cols)
+            #full_df = full_df.set_index('time')
+            logger.info(f"Shape: {full_df.shape}")
+            self.raw_data = full_df
+            return self
+        else:
+            logger.error("No data files were found or loaded.")
+            return None
+
+    def save_to_csv_ohlcv(self, directory="./"):
         if self.raw_data is None or self.raw_data.empty:
             raise ValueError("raw_data is empty. Please fetch data first.")
 
-        filename = f"{self.symbol}_{self.start_date}_{self.end_date}_{self.interval.replace(' ', '')}.csv"
+        filename = f"{self.symbol}_{self.start_date}_{self.end_date}_{self.interval.replace(' ', '')}_ohlcv.csv"
         path = f"{directory.rstrip('/')}/{filename}"
         self.raw_data.to_csv(path, index=False)
         logger.info(f"📁 Data saved to: {path}")
 
-    def load_from_csv_binance(self, directory="./"):
-        required_attrs = [self.symbol, self.start_date, self.end_date, self.interval]
-        if any(attr is None for attr in required_attrs):
-            logger.error("Missing metadata attributes to build the CSV filename.")
-            return
+    def save_to_csv_dom(self, directory="./"):
+        if self.raw_data is None or self.raw_data.empty:
+            raise ValueError("raw_data is empty. Please fetch data first.")
 
-        filename = f"{self.symbol}_{self.start_date}_{self.end_date}_{self.interval.replace(' ', '')}.csv"
+        filename = f"{self.symbol}_{self.start_date}_{self.end_date}_{self.interval.replace(' ', '')}_dom.csv"
+        path = f"{directory.rstrip('/')}/{filename}"
+        self.raw_data.to_csv(path, index=False)
+        logger.info(f"📁 Data saved to: {path}")
+
+    def load_from_csv_ohlcv_binance(self, directory="./"):
+
+        filename = f"{self.symbol}_{self.start_date}_{self.end_date}_{self.interval.replace(' ', '')}_ohlcv.csv"
         path = f"{directory.rstrip('/')}/{filename}"
 
         if not os.path.exists(path):
@@ -107,7 +147,7 @@ class DataFetcher:
 
         try:
             df_raw = pd.read_csv(path, low_memory=False)
-            expected_cols = ['Timestamp', 'Open', 'High', 'Low', 'Close', 'Volume']
+            expected_cols = ['time', 'open', 'high', 'low', 'close', 'volume']
 
             missing = [col for col in expected_cols if col not in df_raw.columns]
             if missing:
@@ -115,9 +155,9 @@ class DataFetcher:
                 return
 
             df = df_raw[expected_cols].copy()
-            df['Timestamp'] = pd.to_datetime(df['Timestamp'], errors='coerce')
-            df = df[df['Timestamp'].notna()]
-            df = df[df['Timestamp'].apply(lambda x: isinstance(x, pd.Timestamp))]
+            df['time'] = pd.to_datetime(df['time'], errors='coerce')
+            df = df[df['time'].notna()]
+            df = df[df['time'].apply(lambda x: isinstance(x, pd.Timestamp))]
 
             if df.empty:
                 logger.error("Loaded CSV file is empty after cleaning.")
@@ -127,7 +167,39 @@ class DataFetcher:
             logger.info(f"📥 Data loaded from: {path}")
         except Exception as e:
             logger.exception(f"Failed to load CSV: {e}")
+            
+    def load_from_csv_dom_bybit(self, directory="./"):
 
+        filename = f"{self.symbol}_{self.start_date}_{self.end_date}_{self.interval.replace(' ', '')}_dom.csv"
+        path = f"{directory.rstrip('/')}/{filename}"
+
+        if not os.path.exists(path):
+            logger.error(f"CSV file not found: {path}")
+            return
+
+        try:
+            df_raw = pd.read_csv(path, low_memory=False)
+            expected_cols = ['time', 'open', 'high', 'low', 'close','ask_size','bid_size']
+
+            missing = [col for col in expected_cols if col not in df_raw.columns]
+            if missing:
+                logger.error(f"Missing columns in CSV: {missing}")
+                return
+
+            df = df_raw[expected_cols].copy()
+            df['time'] = pd.to_datetime(df['time'], errors='coerce')
+            df = df[df['time'].notna()]
+            df = df[df['time'].apply(lambda x: isinstance(x, pd.Timestamp))]
+
+            if df.empty:
+                logger.error("Loaded CSV file is empty after cleaning.")
+                return
+
+            self.raw_data = df
+            logger.info(f"📥 Data loaded from: {path}")
+        except Exception as e:
+            logger.exception(f"Failed to load CSV: {e}")
+            
     def load_from_csv_ib(self, directory="./"):
         required_attrs = [self.symbol, self.start_date, self.end_date, self.interval]
         if any(attr is None for attr in required_attrs):
@@ -160,7 +232,7 @@ class DataFetcher:
         except Exception as e:
             logger.exception(f"Failed to load CSV: {e}")
 
-    def resample_to_1m_ohlcv(self):
+    def resample_to_1m_ohlcv_ib(self):
         df = self.raw_data.copy()
         logger.debug("Starting resample_to_1m...")
 
@@ -199,6 +271,26 @@ class DataFetcher:
         result.dropna(inplace=True)
         logger.info(f"Resampling complete. Final shape: {result.shape}")
 
+        self.raw_data = result
+        return self
+
+    def dom_to_ohlcv(self):
+        # Ensure 'time' is datetime
+        if not pd.api.types.is_datetime64_any_dtype(self.raw_data['time']):
+            self.raw_data['time'] = pd.to_datetime(self.raw_data['time'])
+        # Set 'time' as index if not already
+        if 'time' in self.raw_data.columns:
+            self.raw_data.set_index('time', inplace=True)
+
+        # Resample price to OHLC
+        ohlc = self.raw_data['price'].resample(self.interval).ohlc()
+        # Resample volume for buy and sell sides
+        bid_size = self.raw_data[self.raw_data['side'] == 'buy']['volume'].resample(self.interval).sum().rename(
+            'bid_size')
+        ask_size = self.raw_data[self.raw_data['side'] == 'sell']['volume'].resample(self.interval).sum().rename(
+            'ask_size')
+        # Merge
+        result = pd.concat([ohlc, ask_size, bid_size], axis=1).fillna(0).reset_index()
         self.raw_data = result
         return self
 
