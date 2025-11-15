6d83fb694c399fa3acab21d8b0f6884bfe3aacba
valerian-drmt <valeriandarmente@gmail.com>
2025-11-15 12:16:32 +0100

    Remove deprecated `numpy` from requirements and delete unused `resource/raw/yfinance/wxcwxc` file

--- src/core/ui/main_window.py
+++ src/core/ui/main_window.py
@@ -1,44 +1,26 @@
-from PySide6.QtWidgets import QMainWindow, QWidget, QVBoxLayout
-import core.ui.widgets.candlestick_widget as candlestick_widget
-import pandas as pd
-
 # 🔧 config import
 import os
-from core.config.logger_config import colored_logger
+from src.core.config.logger_config import colored_logger
 logger = colored_logger()
 current_file = os.path.basename(__file__) if '__file__' in globals() else "Notebook"
 logger.info(f"Logger initialized ({current_file})")
 
+import sys
+import PyQt5
+from PyQt5.QtWidgets import QApplication, QWidget
 
-class MainWindow(QMainWindow):
+# Print the location of the PyQt5 installation
+print(PyQt5.__file__)
+
+
+class Window(QWidget):
     def __init__(self):
         super().__init__()
-        logger.info("Initializing MainWindow...")
+        self.setGeometry(50, 50, 300, 450)
+        self.setWindowTitle("Trading Project")
+        self.show()
 
-        self.setWindowTitle("Raw data Candlestick Viewer")
-        self.resize(1200, 800)
 
-        self.candle_widget = candlestick_widget.CandlestickWidget()
-
-        central_widget = QWidget()
-        layout = QVBoxLayout()
-        layout.addWidget(self.candle_widget)
-        central_widget.setLayout(layout)
-
-        self.setCentralWidget(central_widget)
-
-        logger.info("MainWindow successfully initialized.")
-
-    def load_and_plot(self, df: pd.DataFrame):
-        if df is None or df.empty:
-            logger.error("Provided DataFrame is empty or None. Cannot plot candles.")
-            return
-
-        logger.info("Loading DataFrame into candlestick widget...")
-
-        try:
-            self.candle_widget.plot_candles(df)
-            logger.info("Candlestick plot updated successfully.")
-
-        except Exception as e:
-            logger.exception(f"Failed to plot candlesticks: {e}")
+App = QApplication(sys.argv)
+window = Window()
+sys.exit(App.exec())
